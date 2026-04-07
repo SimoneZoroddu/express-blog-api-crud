e01ac7e618d77c9e3b80fc492ead3e0e87cc9f00
SimoneZoroddu <lenviur@gmail.com>
2026-04-07 11:02:04 +0200

    adding files controlls and data posts

---
+++ data/posts.js
@@ -0,0 +1,40 @@
+const posts = [
+    {
+        id: 1,
+        title: 'Introduzione al Blog',
+        content: 'Benvenuti nel mio blog personale dove condivido pensieri e idee.',
+        image: '/images/images.jpg',
+        tags: ['introduzione', 'blog']
+    },
+    {
+        id: 2,
+        title: 'Tecnologia Moderna',
+        content: 'Esplorando le ultime tendenze in tecnologia e sviluppo software.',
+        image: '/images/image2.jpg',
+        tags: ['tecnologia', 'sviluppo']
+    },
+    {
+        id: 3,
+        title: 'Ricette Sane',
+        content: 'Ricette semplici e salutari per la cucina quotidiana.',
+        image: '/images/image3.jpg',
+        tags: ['cucina', 'salute']
+    },
+    {
+        id: 4,
+        title: 'Viaggi e Avventure',
+        content: 'Storie e consigli sui miei viaggi intorno al mondo.',
+        image: '/images/image4.jpg',
+        tags: ['viaggi', 'avventure']
+    },
+    {
+        id: 5,
+        title: 'Fotografia Naturale',
+        content: 'Consigli per catturare la bellezza della natura attraverso la fotografia.',
+        image: '/images/image5.jpg',
+        tags: ['fotografia', 'natura']
+    }
+];
+
+
+module.exports = posts
