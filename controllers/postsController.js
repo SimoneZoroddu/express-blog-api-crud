2c22d1d6e2bd9cedbdf7d3901e36d0b4bc952a63
SimoneZoroddu <lenviur@gmail.com>
2026-04-07 12:19:40 +0200

    adding destroy at the controller

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,35 +1,54 @@
 const myPosts = require("../data/myPosts")
 
-// lista dei post salvati, post                               CREATE
+// lista json di tutti i post, get                                        INDEX
 function index(req, res) {
     res.json(myPosts)
 }
 
-// far vedere il singolo post nell id corrente                INDEX
+// far vedere il singolo post nell id corrente, get/:id                   SHOW
 function show(req, res) {
     const id = parseInt(req.params.id) // parseInt Necessario
     const post = myPosts.filter(post => post.id === id)
     res.json(post)
 }
 
-
-// cambiare tutto l elemento dell id corrente                 UPDATE
+// Creazione nuovi posts, post                                            CREATE
 function store(req, res) {
     res.send(`Creare nuovi posts ${req.params.id}`)
 }
 
-// modificare una piccola parte dell elemento id corrente     UPDATE
+// cambiare tutto l elemento dell id corrente, put/:id                    UPDATE
 function update(req, res) {
     res.send(`cambiare tutto dell elemento id ${req.params.id}`)
 }
 
+// modificare una piccola parte dell elemento id corrente, patch/:id      UPDATE
 function modify(req, res) {
     res.send(`modificare una piccola parte dell id corrente ${req.params.id}`)
 }
 
-//eliminare il singolo elemento id corrente                   DELETE
+//eliminare il singolo elemento id corrente, delete/:id                    DELETE
 function destroy(req, res) {
-    res.send(`rimuovere tutto l elemento id ${req.params.id}`)
+
+    const id = parseInt(req.params.id)
+    const post = myPosts.find(singlePost => singlePost.id === id);
+    
+    if (!post) {
+        return res.status(404).json({
+            status: 404,
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    }
+
+    // Rimuoviamo il post dai posts
+    myPosts.splice(myPosts.indexOf(post), 1);
+    // Rispondiamo al client confermando l'eliminazione
+    return res.status(200).json({
+        status: 200,
+        message: "Post eliminato",
+        data: post
+    })
 }
 
 
--- routers/posts.js
+++ routers/posts.js
@@ -4,22 +4,22 @@ const router = express.Router()
 const postController = require("../controllers/postsController")
 
 
-//lista json di tutti i post, get presi                       INDEX
+//lista json di tutti i post, get                                         INDEX
 router.get("/", postController.index)
 
-// far vedere il singolo post nell id corrente                SHOW
+// far vedere il singolo post nell id corrente, get/:id                   SHOW
 router.get("/:id", postController.show)
 
-// lista dei post salvati, post                               CREATE
+// Creazione nuovi posts, post                                            CREATE
 router.post('/', postController.store)
 
-// cambiare tutto l elemento dell id corrente                 UPDATE
+// cambiare tutto l elemento dell id corrente, put/:id                    UPDATE
 router.put("/:id", postController.update)
 
-// modificare una piccola parte dell elemento id corrente     UPDATE
+// modificare una piccola parte dell elemento id corrente, patch/:id      UPDATE
 router.patch("/:id", postController.modify)
 
-//eliminare il singolo elemento id corrente                   DELETE
+//eliminare il singolo elemento id corrente, delete/:id                   DELETE
 router.delete("/:id", postController.destroy)
 
 module.exports = { router }
